65b38acc0501b8f70bf2da7cdd60befbaccb610e
Markus Reiter <me@reitermark.us>
2015-06-22 11:52:10 +0200

    Don't remove dotfiles repo at the end.

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -21,8 +21,3 @@ if [ -d ~/Documents/Microsoft*/ ]; then
   cecho 'Moving Microsoft folder to Library …' $blue
   mv ~/Documents/Microsoft*/ ~/Library/Preferences/
 fi
-
-
-# Remove Temporary Git Repo
-cecho 'Removing “dotfiles” Repository …' $blue
-rm -rf $git_dir
