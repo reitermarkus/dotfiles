3f252be730ab1a8c92817e45021c7dc69c8a66c8
Markus Reiter <me@reitermark.us>
2015-06-29 11:05:09 +0200

    Add script enable assistive access for Terminal.

--- .sh
+++ .sh
@@ -34,6 +34,18 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
+# Grant Terminal access to Assistive Features
+
+sudo python <<EOF
+import sqlite3
+conn = sqlite3.connect('/Library/Application Support/com.apple.TCC/TCC.db')
+conn.cursor().execute("INSERT or REPLACE INTO access VALUES('kTCCServiceAccessibility','com.apple.Terminal',0,1,1,NULL,NULL)")
+conn.cursor().execute("INSERT or REPLACE INTO access VALUES('kTCCServiceAccessibility','$(which osascript)',1,1,1,NULL,NULL)")
+conn.commit()
+conn.close()
+EOF
+
+
 # Run Scripts
 source "$git_dir/setup/defaults.sh"
 source "$git_dir/setup/app-store.sh"
