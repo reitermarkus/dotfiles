52e52deb77385572a53523bbf9216e6116d31603
Markus Reiter <me@reitermark.us>
2015-10-08 21:36:25 +0200

    Fix node script.

--- setup/node.sh
+++ setup/node.sh
@@ -4,15 +4,18 @@
 # Install NPM Packages
 
 npm_install_if_missing() {
-  if hash "$1"; then
-    cecho "“$1” already installed." $green
+  
+  if npm -g ls | awk '{ print $NF }' | grep "^$1@" &>/dev/null; then
+    echo_exists "$1"
   else
-    if hash npm; then
-      cecho 'Installing “$1” …'
-      npm install -g "$1"
-    fi
+    echo_install "$1"
+    npm install -g "$1"
   fi
 }
 
-brew cask ls imageoptim &>/dev/null && npm_install_if_missing imageoptim
-npm_install_if_missing svgexport
+if hash npm; then
+
+  brew cask ls imageoptim &>/dev/null && npm_install_if_missing imageoptim
+  npm_install_if_missing svgexport
+
+fi
