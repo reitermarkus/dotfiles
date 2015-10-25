c9547ff3c175242223ef98f23fd56591fd338ac3
Markus Reiter <me@reitermark.us>
2015-10-25 22:06:23 +0100

    Illustrator is now named CC 2015.

--- setup/dock.sh
+++ setup/dock.sh
@@ -59,20 +59,20 @@ if hash dockutil; then
   dutil -rn Feedback\ Assistant
   dutil -rn Feedback-Assistent &>/dev/null
 
-  dutil -n Launchpad                  -p /Applications/Launchpad.app
-  dutil -n Safari                     -p /Applications/Safari.app
-  dutil -n Mail                       -p /Applications/Mail.app
-  dutil -n Reeder                     -p /Applications/Reeder.app
-  dutil -n Notizen                    -p /Applications/Notes.app
-  dutil -n Nachrichten                -p /Applications/Messages.app
-  dutil -n iTunes                     -p /Applications/iTunes.app
-  dutil -n Fotos                      -p /Applications/Photos.app
-  dutil -n iBooks                     -p /Applications/iBooks.app
-  dutil -n Pages                      -p /Applications/Pages.app
-  dutil -n Numbers                    -p /Applications/Numbers.app
-  dutil -n TextMate                   -p /Applications/TextMate.app
-  dutil -n Tower                      -p /Applications/Tower.app
-  dutil -n Adobe\ Photoshop\ CC\ 2015 -p /Applications/Adobe\ Photoshop\ CC\ 2015/Adobe\ Photoshop\ CC\ 2015.app
-  dutil -ln Adobe\ Illustrator        -p /Applications/Adobe\ Illustrator\ CC\ 2015/Adobe\ Illustrator.app
+  dutil -n Launchpad                     -p /Applications/Launchpad.app
+  dutil -n Safari                        -p /Applications/Safari.app
+  dutil -n Mail                          -p /Applications/Mail.app
+  dutil -n Reeder                        -p /Applications/Reeder.app
+  dutil -n Notizen                       -p /Applications/Notes.app
+  dutil -n Nachrichten                   -p /Applications/Messages.app
+  dutil -n iTunes                        -p /Applications/iTunes.app
+  dutil -n Fotos                         -p /Applications/Photos.app
+  dutil -n iBooks                        -p /Applications/iBooks.app
+  dutil -n Pages                         -p /Applications/Pages.app
+  dutil -n Numbers                       -p /Applications/Numbers.app
+  dutil -n TextMate                      -p /Applications/TextMate.app
+  dutil -n Tower                         -p /Applications/Tower.app
+  dutil -n Adobe\ Photoshop\ CC\ 2015    -p /Applications/Adobe\ Photoshop\ CC\ 2015/Adobe\ Photoshop\ CC\ 2015.app
+  dutil -ln Adobe\ Illustrator\ CC\ 2015 -p /Applications/Adobe\ Illustrator\ CC\ 2015/Adobe\ Illustrator.app
 
 fi
