a35edab45c46fd2ba7038f16c04f2881e5164ffe
Markus Reiter <me@reitermark.us>
2015-11-15 04:29:37 +0100

    Add Desktop Pictures and fix Dropbox script.

--- include/5.0-dropbox.sh
+++ include/5.0-dropbox.sh
@@ -12,56 +12,48 @@ link_to_dropbox() {
   # Also move hidden files.
   shopt -s dotglob
 
-  local dropbox_dir
-  local local_dir
+  local local_dir="${HOME}/${1}"
+  local dropbox_dir="$(get_dropbox_dir)"
 
-  local dropbox_dir=$(get_dropbox_dir)
-  [[ -n "${dropbox_dir}" ]] || return 1
+  if [ -d "${dropbox_dir}" ]; then
 
-  dropbox_dir="$dropbox_dir/Sync/~/$1"
+    dropbox_dir="${dropbox_dir}/Sync/~/${1}"
 
-  if [[ -z "${2}" ]]; then
-    local_dir="~/${1}"
-  else
-    local_dir="~/${2}"
-  fi
-
-  eval dropbox_dir_full=$dropbox_dir
-  eval local_dir_full=$local_dir
-
-  if [[ -L "$dropbox_dir_full" ]]; then
-    echo -g "${local_dir} already linked to Dropbox."
-  else
-    echo -b "Linking ${local_dir} to ${dropbox_dir} …"
-
-    killall Dropbox &>/dev/null
-
-    rmdir "$dropbox_dir_full" "$local_dir_full" &>/dev/null
-    rm -f "$local_dir_full"
-
-    if [ ! -d "$local_dir_full" ] && [ -d "$dropbox_dir_full" ]; then
-      mv -f "$dropbox_dir_full" "$local_dir_full"
-    elif [ -d "$local_dir_full" ] && [ -d "$dropbox_dir_full" ]; then
-      mv -f "$dropbox_dir_full"/* "$local_dir_full"/
-      rmdir "$dropbox_dir_full"
-    elif [ ! -d "$local_dir_full" ] && [ ! -d "$dropbox_dir_full" ]; then
-      mkdir -p "$local_dir_full"
+    if [ "${2}" != '' ]; then
+      local_dir="${HOME}/${2}"
     fi
 
-    if [ -d "$local_dir_full" ] && [ ! -d "$dropbox_dir_full" ]; then
-      ln -sfn "$local_dir_full" "$dropbox_dir_full" || echo -r "Error linking $local_dir to $dropbox_dir."
+    local local_dirname="$(sed "s|${HOME}|~|" <<< "${local_dir}")"
+    local dropbox_dirname="$(sed "s|${HOME}|~|" <<< "${dropbox_dir}")"
+
+    if [ -L "${dropbox_dir}" ]; then
+      echo -g "${local_dirname} already linked to Dropbox."
+    else
+      echo -b "Linking ${local_dirname} to ${dropbox_dirname} …"
+
+      killall Dropbox &>/dev/null
+
+      rmdir "${dropbox_dir}" &>/dev/null
+      rm -f "${dropbox_dir}" "${local_dir}" &>/dev/null
+      mkdir -p "${local_dir}"
+
+      if [ -d "${dropbox_dir}" ]; then
+        mv -f "${dropbox_dir}"/* "${local_dir}"/
+        rmdir "${dropbox_dir}"
+      fi
+
+      if [ -d "${local_dir}" ] && [ ! -d "${dropbox_dir}" ]; then
+        ln -sfn "${local_dir}" "${dropbox_dir}" || echo -r "Error linking ${local_dirname} to ${dropbox_dirname}."
+      fi
+
     fi
-
-    open -gja Dropbox
-
   fi
-
 }
 
 
 dropbox_link_folders() {
 
-  # Check if Dropbox has finishes syncing.
+  # Check if Dropbox has finished syncing.
 
   echo -b 'Waiting for Dropbox to finish syncing …'
   until osascript -e 'tell application "System Events" to tell application process "Dropbox" to get help of menu bar item 1 of menu bar 2' | grep --quiet -E 'Aktualisiert|Up to date'; do
@@ -73,6 +65,7 @@ dropbox_link_folders() {
 
   link_to_dropbox 'Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks'
   link_to_dropbox 'Library/Fonts'
+  link_to_dropbox 'Library/Desktop Pictures'
 
   link_to_dropbox 'Documents/Backups'
   link_to_dropbox 'Documents/Cinquecento'
@@ -93,6 +86,8 @@ dropbox_link_folders() {
     mv "$(get_dropbox_dir)/Sync/~/${favorites}" "${HOME}/${favorites}"
     ln -s "${HOME}/${favorites}" "$(get_dropbox_dir)/Sync/~/${favorites}"
   fi
+
+  open -gja Dropbox
 
 }
 
