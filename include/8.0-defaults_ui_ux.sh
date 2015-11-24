16aacc04a58ace311a17e607d1280d6e31f49de6
Markus Reiter <me@reitermark.us>
2015-11-24 23:38:10 +0100

    Non-Floating Mode for Help Viewer

--- include/8.0-defaults_ui_ux.sh
+++ include/8.0-defaults_ui_ux.sh
@@ -8,16 +8,18 @@ defaults_ui_ux() {
   echo -b 'Setting defaults for User Interface …'
 
   # Expand Save Panel by Default
-  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode  -bool true
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint     -bool true
+  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2    -bool true
 
   # Keep Open Windows on Quit
   defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool true
 
-
   # Disable Font Smoothing
   defaults write NSGlobalDomain AppleFontSmoothing -int 0
 
+  # Enable Help Viewer Non-Floating Mode
+  defaults write com.apple.helpviewer DevMode -bool true
 
 }
