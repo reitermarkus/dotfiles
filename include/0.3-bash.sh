1484e025d7b8abc52fedfd3225303d51c8cfb95f
Markus Reiter <me@reitermark.us>
2015-12-02 12:02:26 +0100

    Simplify Bash setup script.

--- include/0.3-bash.sh
+++ include/0.3-bash.sh
@@ -6,34 +6,36 @@
 install_bash_shell() {
 
     local bash_bin=$(which bash)
+    local line
 
     # Check if “bash” is in shells file.
     if ! grep --quiet "${bash_bin}" /etc/shells; then
       echo "${bash_bin}" | sudo tee -a /etc/shells &>/dev/null
     fi
 
-    touch \
-      "${HOME}/.bash_profile" \
-      "${HOME}/.profile" \
-      "${HOME}/.bashrc" \
-    ;
+    local bash_profile="${HOME}/.bash_profile"
+    local bashrc="${HOME}/.bashrc"
+    local profile="${HOME}/.profile"
+
+    touch "${bash_profile}" "${bashrc}" "${profile}"
+
+    add_to_config() {
+      local config="${1}"
+      local line="${2}"
+
+      if ! grep --quiet "${line}" "${config}"; then
+        echo "${line}" >> "${config}"
+      fi
+    }
 
     # Import ~/.profile into ~/.bash_profile
-    local bash_profile_source_profile='[[ -f "${HOME}/.profile" ]] && source "${HOME}/.profile"'
-    if ! grep --quiet "${bash_profile_source_profile}" "${HOME}/.bash_profile"; then
-      echo "${bash_profile_source_profile}" >> "${HOME}/.bash_profile"
-    fi
+    add_to_config "${bash_profile}" 'test -f "${HOME}/.profile" && source "${HOME}/.profile"'
 
     # Import ~/.bashrc into ~/.bash_profile
-    local bash_profile_source_bashrc='[[ -f "${HOME}/.bashrc" ]] && source "${HOME}/.bashrc"'
-    if ! grep --quiet "${bash_profile_source_bashrc}" "${HOME}/.bash_profile"; then
-      echo "${bash_profile_source_bashrc}" >> "${HOME}/.bash_profile"
-    fi
+    add_to_config "${bash_profile}" 'test -f "${HOME}/.bashrc" && source "${HOME}/.bashrc"'
 
     # Import Bash Completion into ~/.bashrc
-    local bashrc_source_bash_completion='hash brew && test -f "$(brew --prefix)/etc/bash_completion" && source "$(brew --prefix)/etc/bash_completion"'
-    if ! grep --quiet "${bashrc_source_bash_completion}" "${HOME}/.bashrc"; then
-      echo "${bashrc_source_bash_completion}" >> "${HOME}/.bashrc"
-    fi
+    add_to_config "${bashrc}" 'test -f /etc/bashrc && source /etc/bashrc'
+    add_to_config "${bashrc}" 'brew ls bash-completion &>/dev/null && source "$(brew --prefix)/etc/bash_completion"'
 
 }
