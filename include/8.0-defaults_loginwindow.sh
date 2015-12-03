7b043ba6a787392a9d69a0e1865ff327edd45081
Markus Reiter <me@reitermark.us>
2015-12-03 19:41:44 +0100

    Add LaunchDaemon to log out Guest user when idle.

--- include/8.0-defaults_loginwindow.sh
+++ include/8.0-defaults_loginwindow.sh
@@ -13,6 +13,9 @@ defaults_loginwindow() {
   sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool false
   sudo rm -rf /Users/Guest
 
+  # Automatically log out the Guest when idle.
+  install_launchdaemon_logout_guest_on_idle
+
   # Show the Sleep, Restart and Shut Down Buttons
   sudo defaults write /Library/Preferences/com.apple.loginwindow 'PowerOffDisabled' -bool false
 
@@ -54,5 +57,43 @@ defaults_loginwindow() {
 
   sudo chown "${USER}:staff" "${USER_PICTURE}"
   sudo chmod a=r,u+w "${USER_PICTURE}"
+
+}
+
+
+install_launchdaemon_logout_guest_on_idle() {
+
+  launchd_name='com.apple.LogoutGuestOnIdle'
+  launchd_plist="/Library/LaunchDaemons/${launchd_name}.plist"
+
+  sudo rm -f "${launchd_plist}"
+
+  sudo echo '''
+    <plist>
+      <dict>
+        <key>ProgramArguments</key>
+        <array>
+          <string>/bin/bash</string>
+          <string>-c</string>
+          <string>
+            GUI_USER="$(stat -f %Su /dev/console)";
+            if [ "$GUI_USER" == "Guest" ] &amp;&amp; [ "$(ioreg -c IOHIDSystem | awk "/HIDIdleTime/ {print \$NF/1000000000; exit}" | sed -E "s/(\,|\.).*//")" -ge 300 ]; then
+              ps awwwux | awk "/$GUI_USER/ &amp;&amp; /[l]oginwindow/ { print \$2 }" | xargs kill -9;
+            fi
+          </string>
+        </array>
+      </dict>
+    </plist>
+  ''' > "${launchd_plist}"
+
+  sudo defaults write "${launchd_plist}" Label -string "${launchd_name}"
+  sudo defaults write "${launchd_plist}" RunAtLoad -bool true
+  sudo defaults write "${launchd_plist}" StartInterval -int 10
+
+  sudo chown root:admin "${launchd_plist}"
+  sudo chmod 755 "${launchd_plist}"
+
+  sudo launchctl unload "${launchd_plist}" &>/dev/null
+  sudo launchctl load "${launchd_plist}"
 
 }
