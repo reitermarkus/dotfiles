aefc40f1439763aa759d460daef0d31ff6c5f3f2
Markus Reiter <me@reitermark.us>
2015-12-06 18:58:40 +0100

    Reorder Third Party Defaults.

--- include/8.0-defaults_third_party_apps.sh
+++ include/8.0-defaults_third_party_apps.sh
@@ -5,29 +5,6 @@
 
 defaults_third_party_apps() {
 
-  # SaveHollywood Screensaver
-  if app_installed fr.whitebox.SaveHollywood && has_enough_space; then
-    defaults -currentHost write com.apple.screensaver moduleDict -dict-add moduleName -string 'SaveHollywood'
-    defaults -currentHost write com.apple.screensaver moduleDict -dict-add path       -string "$(mdfind -onlyin / kMDItemCFBundleIdentifier==fr.whitebox.SaveHollywood)"
-    defaults -currentHost write com.apple.screensaver moduleDict -dict-add type          -int 0
-
-    python "${dotfiles_dir}/scripts/download-apple-tv-screensavers.py"
-
-    defaults -currentHost write fr.whitebox.SaveHollywood assets.library               -array "${HOME}/Library/Screen Savers/Videos"
-    defaults -currentHost write fr.whitebox.SaveHollywood assets.randomOrder            -bool true
-    defaults -currentHost write fr.whitebox.SaveHollywood assets.startWhereLeftOff      -bool false
-    defaults -currentHost write fr.whitebox.SaveHollywood frame.drawBorder              -bool false
-    defaults -currentHost write fr.whitebox.SaveHollywood frame.randomPosition          -bool false
-    defaults -currentHost write fr.whitebox.SaveHollywood frame.scaling                  -int 1
-    defaults -currentHost write fr.whitebox.SaveHollywood frame.showMetadata.mode       -bool false
-    defaults -currentHost write fr.whitebox.SaveHollywood movie.volume.mode              -int 1
-    defaults -currentHost write fr.whitebox.SaveHollywood screen.mainDisplayOnly        -bool false
-  else
-    defaults -currentHost write com.apple.screensaver moduleDict -dict-add moduleName -string 'Arabesque'
-    defaults -currentHost write com.apple.screensaver moduleDict -dict-add path       -string '/System/Library/Screen Savers/Arabesque.qtz'
-    defaults -currentHost write com.apple.screensaver moduleDict -dict-add type          -int 1
-  fi
-
   # BetterSnapTool
   if app_installed com.hegenberg.BetterSnapTool; then
     add_app_to_tcc com.hegenberg.BetterSnapTool
@@ -86,6 +63,29 @@ defaults_third_party_apps() {
   # Parallels Desktop
   if app_installed com.parallels.desktop.console; then
     replace_parallels_icon &
+  fi
+
+  # SaveHollywood Screensaver
+  if app_installed fr.whitebox.SaveHollywood && has_enough_space; then
+    defaults -currentHost write com.apple.screensaver moduleDict -dict-add moduleName -string 'SaveHollywood'
+    defaults -currentHost write com.apple.screensaver moduleDict -dict-add path       -string "$(mdfind -onlyin / kMDItemCFBundleIdentifier==fr.whitebox.SaveHollywood)"
+    defaults -currentHost write com.apple.screensaver moduleDict -dict-add type       -int    0
+
+    python "${dotfiles_dir}/scripts/download-apple-tv-screensavers.py"
+
+    defaults -currentHost write fr.whitebox.SaveHollywood assets.library               -array "${HOME}/Library/Screen Savers/Videos"
+    defaults -currentHost write fr.whitebox.SaveHollywood assets.randomOrder           -bool  true
+    defaults -currentHost write fr.whitebox.SaveHollywood assets.startWhereLeftOff     -bool  false
+    defaults -currentHost write fr.whitebox.SaveHollywood frame.drawBorder             -bool  false
+    defaults -currentHost write fr.whitebox.SaveHollywood frame.randomPosition         -bool  false
+    defaults -currentHost write fr.whitebox.SaveHollywood frame.scaling                -int   1
+    defaults -currentHost write fr.whitebox.SaveHollywood frame.showMetadata.mode      -bool  false
+    defaults -currentHost write fr.whitebox.SaveHollywood movie.volume.mode            -int   1
+    defaults -currentHost write fr.whitebox.SaveHollywood screen.mainDisplayOnly       -bool  false
+  else
+    defaults -currentHost write com.apple.screensaver moduleDict -dict-add moduleName -string 'Arabesque'
+    defaults -currentHost write com.apple.screensaver moduleDict -dict-add path       -string '/System/Library/Screen Savers/Arabesque.qtz'
+    defaults -currentHost write com.apple.screensaver moduleDict -dict-add type       -int    1
   fi
 
   # Tower
