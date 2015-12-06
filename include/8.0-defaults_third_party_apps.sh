49134c44dd5d9ef69ae95a2f1debe1071b0bfa37
Markus Reiter <me@reitermark.us>
2015-12-06 19:03:13 +0100

    Add LaunchBar defaults.

--- include/8.0-defaults_third_party_apps.sh
+++ include/8.0-defaults_third_party_apps.sh
@@ -53,6 +53,14 @@ defaults_third_party_apps() {
   if app_installed at.obdev.LaunchBar; then
     add_app_to_tcc at.obdev.LaunchBar at.obdev.LaunchBar-AppleScript-Runner
     add_login_item at.obdev.LaunchBar hidden
+
+    defaults write at.obdev.LaunchBar Autohide                    -bool   true
+    defaults write at.obdev.LaunchBar AppendEnglishFilename       -bool   true
+    defaults write at.obdev.LaunchBar LaunchBarWindowWidth        -int    680
+    defaults write at.obdev.LaunchBar LaunchBarHorizontalPosition -float  '0.5'
+    defaults write at.obdev.LaunchBar LaunchBarVerticalPosition   -float  '0.805'
+    defaults write at.obdev.LaunchBar ShowDockIcon                -bool   false
+    defaults write at.obdev.LaunchBar Theme                       -string 'at.obdev.LaunchBar.theme.Yosemite'
   fi
 
   # Keka
