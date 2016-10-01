4469f44e8a69aadbaedff542143a4b4c22b5c4f3
Markus Reiter <me@reitermark.us>
2016-10-01 04:30:18 +0200

    Dictionaries are stored ”in the Cloud“ on Sierra.

--- include/cleanup/cleanup.sh
+++ include/cleanup/cleanup.sh
@@ -15,11 +15,6 @@ cleanup() {
   echo -r 'Emptying CoreSymbolication cache …'
   sudo /bin/rm -rfv /System/Library/Caches/com.apple.coresymbolicationd/data | /usr/bin/xargs -0 printf 'Removing: %s\n'
 
-  echo -r 'Removing unneeded Dictionaries …'
-  /usr/bin/find -E /Library/Dictionaries -depth 1 -iregex \
-    '.*(Chinese|Dutch|French|française|Hindi|Japanese|Daijirin|Korean|Norwegian|Portuguese|Russian|Spanish|Española|Swedish|Thai|Turkish).*' \
-    -print0 | /usr/bin/xargs -0 sudo /bin/rm -rf | /usr/bin/xargs -0 printf 'Removing: %s\n'
-
   if type remove_dotfiles_dir &>/dev/null; then remove_dotfiles_dir; fi
 
 }
