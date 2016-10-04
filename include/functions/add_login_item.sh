85e6e845bc79df32e40031b3aeaf063aae153a67
Markus Reiter <me@reitermark.us>
2016-10-04 11:04:25 +0200

    Use JavaScript (JXA) to add login items.

--- include/functions/add_login_item.sh
+++ include/functions/add_login_item.sh
@@ -9,11 +9,26 @@ add_login_item() {
   fi
 
   if is_app_installed "${bundle_id}"; then
-    /usr/bin/osascript \
-      -e 'tell application "System Events"' \
-      -e "make login item with properties {path:\"$(/usr/bin/mdfind -onlyin / kMDItemCFBundleIdentifier=="${bundle_id}")\", hidden:${hidden}}" \
-      -e 'end tell' \
-      -e 'return'
+
+    local path="$(/usr/bin/mdfind -onlyin / kMDItemCFBundleIdentifier=="${bundle_id}")"
+
+    /usr/bin/osascript -l JavaScript \
+      -e "'use strict';                                   " \
+      -e "                                                " \
+      -e "ObjC.import('stdlib')                           " \
+      -e "                                                " \
+      -e "var systemEvents = Application('System Events') " \
+      -e "                                                " \
+      -e "var newLoginItem = systemEvents.LoginItem({     " \
+      -e "  path: '${path}',                              " \
+      -e "  hidden: ${hidden}                             " \
+      -e "})                                              " \
+      -e "                                                " \
+      -e "systemEvents.loginItems.push(newLoginItem)      " \
+      -e "                                                " \
+      -e "$.exit(0)                                       " \
+      -e "                                                "
+
   fi
 
 }
