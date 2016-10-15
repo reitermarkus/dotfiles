82ceec3f0212482789d891abd4de2495cf9c83c2
Markus Reiter <me@reitermark.us>
2016-10-16 01:42:15 +0200

    Use absolute paths to programs.

--- include/dependencies/install_xcode_clt.sh
+++ include/dependencies/install_xcode_clt.sh
@@ -1,13 +1,13 @@
 install_xcode_clt() {
 
-  if xcode-select --print-path &>/dev/null; then
+  if /usr/bin/xcode-select --print-path &>/dev/null; then
     echo -g 'Command Line Developer Tools are already installed.'
   else
     echo -b 'Installing Command Line Developer Tools …'
 
     /usr/bin/touch '/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress'
-    local CLDT=$(softwareupdate --list | /usr/bin/grep "\*.*Command Line" | /usr/bin/head -1 | /usr/bin/sed -E 's/^[\t \*]*(.*)[\t ]*$/\1/' | tr -d '\n')
-    softwareupdate --verbose --install "${CLDT}" | /usr/bin/grep '%'
+    local CLDT="$(/usr/sbin/softwareupdate --list | /usr/bin/grep "\*.*Command Line" | /usr/bin/head -1 | /usr/bin/sed -E 's/^[\t \*]*(.*)[\t ]*$/\1/' | /usr/bin/tr -d '\n')"
+    /usr/sbin/softwareupdate --verbose --install "${CLDT}" | /usr/bin/grep '%'
   fi
 
 }
