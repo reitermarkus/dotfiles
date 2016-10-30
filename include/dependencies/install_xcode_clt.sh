a4fe7039c949a963be035f698be4ae55d896e315
Markus Reiter <me@reitermark.us>
2016-10-30 02:07:45 +0100

    Always install latest Command Line Developer Tools.

--- include/dependencies/install_xcode_clt.sh
+++ include/dependencies/install_xcode_clt.sh
@@ -6,8 +6,8 @@ install_xcode_clt() {
     echo -b 'Installing Command Line Developer Tools …'
 
     /usr/bin/touch '/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress'
-    local CLDT="$(/usr/sbin/softwareupdate --list | /usr/bin/grep "\*.*Command Line" | /usr/bin/head -1 | /usr/bin/sed -E 's/^[\t \*]*(.*)[\t ]*$/\1/' | /usr/bin/tr -d '\n')"
-    /usr/sbin/softwareupdate --verbose --install "${CLDT}" | /usr/bin/grep '%'
+    local CLDT="$(/usr/sbin/softwareupdate --list | /usr/bin/sed -E '/^.*\*\ *(Command Line Tools.*)\ *$/h;g;$!d;s//\1/')"
+    /usr/sbin/softwareupdate --verbose --install "${CLDT}"
   fi
 
 }
