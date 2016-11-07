ea17470072490cbea5eab8bd9cd1526c1f9e782f
Markus Reiter <me@reitermark.us>
2016-11-07 06:34:09 +0100

    Don’t output `Cleaning up …`.

--- include/cleanup/cleanup.sh
+++ include/cleanup/cleanup.sh
@@ -1,7 +1,5 @@
 cleanup() {
 
-  echo -r 'Cleaning up …'
-
   echo -r 'Emptying CoreSymbolication cache …'
   /usr/bin/sudo -E -- /bin/rm -rfv /System/Library/Caches/com.apple.coresymbolicationd/data | /usr/bin/xargs -0 printf 'Removing: %s\n'
 
