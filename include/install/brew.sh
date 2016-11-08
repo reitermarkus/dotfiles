7701049b46ed7df7cdde79ee014078662e39fb68
Markus Reiter <me@reitermark.us>
2016-11-08 22:33:09 +0100

    `sshfs` depends on FUSE.

--- include/install/brew.sh
+++ include/install/brew.sh
@@ -170,6 +170,7 @@ install_brew_formulae() {
     brew_install -p python            -n 'Python 2'; if type pip2 &>/dev/null; then pip2 install --upgrade pip setuptools; fi
     brew_install -p python3           -n 'Python 3'; if type pip3 &>/dev/null; then pip3 install --upgrade pip setuptools; fi
     brew_install -p ruby              -n 'Ruby'
+    brew_install -c osxfuse           -n 'FUSE' && \
     brew_install -p sshfs             -n 'SSHFS'
     brew_install -p terminal-notifier -n 'Terminal Notifier'
     brew_install -p trash             -n 'Trash'
