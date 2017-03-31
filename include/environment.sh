1adc905ba2bad2c04690532a463f971bab4a9aeb
Markus Reiter <me@reitermark.us>
2017-04-01 01:07:31 +0200

    Fix multi-line `HOMEBREW_CASK_OPTS` not getting set in `fish`.

--- include/environment.sh
+++ include/environment.sh
@@ -23,13 +23,6 @@ export BUNDLE_PATH="$HOME/.config/bundle"
 
 # Homebrew
 
-export HOMEBREW_CASK_OPTS='''
-  --appdir=/Applications
-  --dictionarydir=/Library/Dictionaries
-  --colorpickerdir=/Library/ColorPickers
-  --prefpanedir=/Library/PreferencePanes
-  --qlplugindir=/Library/QuickLook
-  --screen_saverdir=/Library/Screen\ Savers
-'''
+export HOMEBREW_CASK_OPTS='--appdir=/Applications --dictionarydir=/Library/Dictionaries --colorpickerdir=/Library/ColorPickers --prefpanedir=/Library/PreferencePanes --qlplugindir=/Library/QuickLook --screen_saverdir=/Library/Screen\ Savers'
 export HOMEBREW_DEVELOPER='1'
 export HOMEBREW_GITHUB_API_TOKEN='0e3924d278578977b3bd88980c71877e0c426184'
