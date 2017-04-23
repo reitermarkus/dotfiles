256b54276cf681cb3e8a7694fe6dd7dc0219b5ee
Markus Reiter <me@reitermark.us>
2017-04-24 01:03:42 +0200

    Add `git sync` alias.

--- include/settings/git.sh
+++ include/settings/git.sh
@@ -42,6 +42,8 @@ defaults_git() {
 
   git config --global alias.master '! f() { git fetch ${1:-origin} master && git rebase ${1:-origin}/master; }; f'
 
+  git config --global alias.sync '! f() { git pull ${@} && git push ${@}; }; f'
+
   git config --global alias.new 'checkout -b'
   git config --global alias.shove 'push --force'
 
