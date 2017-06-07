4582afb9323af7eee135df8f2a23cdd4fa9c8e1e
Markus Reiter <me@reitermark.us>
2017-06-07 10:32:49 +0200

    Fix TextMate environment variables.

--- include/settings/textmate.sh
+++ include/settings/textmate.sh
@@ -5,13 +5,21 @@
 
 defaults_textmate() {
 
-  /usr/bin/defaults write com.macromates.TextMate environmentVariables -array """
-    <dict>
-      <key>enabled</key><true/>
-      <key>name</key><string>PATH</string>
-      <key>value</key><string>$RBENV_ROOT/shims:\$PATH:/usr/local/bin</string>
-    </dict>
-  """
+  /usr/bin/defaults write com.macromates.TextMate environmentVariables -array \
+    """
+      <dict>
+        <key>enabled</key><true/>
+        <key>name</key><string>RBENV_ROOT</string>
+        <key>value</key><string>$RBENV_ROOT</string>
+      </dict>
+    """ \
+    """
+      <dict>
+        <key>enabled</key><true/>
+        <key>name</key><string>PATH</string>
+        <key>value</key><string>\$RBENV_ROOT/shims:\$PATH:/usr/local/bin</string>
+      </dict>
+    """
 
   /usr/bin/killall cfprefsd &>/dev/null
 
