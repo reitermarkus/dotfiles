7bfaf9cc551e412c9dd7017c29786598a04b540b
Markus Reiter <me@reitermark.us>
2017-07-23 16:37:15 +0200

    Add PlayNow.

--- include/install/brew.sh
+++ include/install/brew.sh
@@ -167,6 +167,7 @@ install_brew_casks() {
   brew cask install macdown
   brew cask install mactex
   brew cask install netspot
+  brew cask install playnow
   brew cask install prizmo
   brew cask install postman
   brew cask install qlmarkdown
