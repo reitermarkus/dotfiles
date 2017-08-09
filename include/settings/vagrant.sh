ec1bebf08950d6930da49855b24b7d5649394516
Markus Reiter <me@reitermark.us>
2017-08-09 18:42:17 +0200

    Allow sudo-less `vagrant-hostsupdater`.

--- include/settings/vagrant.sh
+++ include/settings/vagrant.sh
@@ -10,4 +10,11 @@ defaults_vagrant() {
     fi
   fi
 
+  {
+    echo '# Allow passwordless startup of Vagrant with vagrant-hostsupdater.';
+    echo 'Cmnd_Alias VAGRANT_HOSTS_ADD = /bin/sh -c echo "*" >> /etc/hosts';
+    echo 'Cmnd_Alias VAGRANT_HOSTS_REMOVE = /usr/bin/sed -i -e /*/ d /etc/hosts';
+    echo '%admin ALL=(root) NOPASSWD: VAGRANT_HOSTS_ADD, VAGRANT_HOSTS_REMOVE';
+  } | sudo -E -- /usr/bin/tee /etc/sudoers.d/vagrant_hostsupdater
+
 }
