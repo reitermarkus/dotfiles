c69482d394908dd0c7f19ffe069bfc79902b5741
Markus Reiter <me@reitermark.us>
2017-09-16 03:40:39 +0200

    Assume `mas` installation succeeded.

--- include/install/appstore.sh
+++ include/install/appstore.sh
@@ -17,28 +17,24 @@ mas_install() {
 
 install_mas_apps() {
 
-  if which mas &>/dev/null; then
+  echo -b 'Updating App Store Applications …'
+  mas upgrade
 
-    echo -b 'Updating App Store Applications …'
-    mas upgrade
-
-    mas_install 608292802 'Auction Sniper for eBay'
-    mas_install 417375580 'BetterSnapTool'
-    mas_install 420212497 'Byword'
-    mas_install 924726344 'Deliveries'
-    mas_install 480664966 'Fusio'
-    mas_install 463541543 'Gemini'
-    mas_install 402989379 'iStudiez Pro'
-    mas_install 409183694 'Keynote'
-    mas_install 482898991 'LiveReload'
-    mas_install 409203825 'Numbers'
-    mas_install 409201541 'Pages'
-    mas_install 419891002 'RapidClick'
-    mas_install 954196690 'RegexToolbox'
-    mas_install 443370764 'Repeater'
-    mas_install 497799835 'Xcode'; sudo -E -- xcodebuild -license accept &>/dev/null
-    mas_install 892115848 'yRegex'
-
-  fi
+  mas_install 608292802 'Auction Sniper for eBay'
+  mas_install 417375580 'BetterSnapTool'
+  mas_install 420212497 'Byword'
+  mas_install 924726344 'Deliveries'
+  mas_install 480664966 'Fusio'
+  mas_install 463541543 'Gemini'
+  mas_install 402989379 'iStudiez Pro'
+  mas_install 409183694 'Keynote'
+  mas_install 482898991 'LiveReload'
+  mas_install 409203825 'Numbers'
+  mas_install 409201541 'Pages'
+  mas_install 419891002 'RapidClick'
+  mas_install 954196690 'RegexToolbox'
+  mas_install 443370764 'Repeater'
+  mas_install 497799835 'Xcode'; sudo -E -- xcodebuild -license accept &>/dev/null
+  mas_install 892115848 'yRegex'
 
 }
