74cbd83bebfcfd766dae61eec04b77daaac9d4d5
Markus Reiter <me@reitermark.us>
2017-11-28 23:00:03 +0100

    `.dev` is a TLD.

--- include/settings/dnsmasq.sh
+++ include/settings/dnsmasq.sh
@@ -2,14 +2,14 @@ defaults_dnsmasq() {
 
   local dnsmasq_conf="$(brew --prefix)/etc/dnsmasq.conf"
 
-  if ! /usr/bin/grep --quiet '^address=/dev/127.0.0.1$' "${dnsmasq_conf}"; then
+  if ! /usr/bin/grep --quiet '^address=/.localhost/127.0.0.1$' "${dnsmasq_conf}"; then
     sed -i '' -E
       '/^#address=\/double-click.net\/127.0.0.1/a\
-       address=/dev/127.0.0.1\
+       address=/.localhost/127.0.0.1\
       ' "${dnsmasq_conf}"
   fi
 
   sudo /bin/mkdir -p /etc/resolver
-  echo "nameserver 127.0.0.1" | sudo -E -- /usr/bin/tee /etc/resolver/dev >/dev/null
+  echo "nameserver 127.0.0.1" | sudo -E -- /usr/bin/tee /etc/resolver/localhost >/dev/null
 
 }
