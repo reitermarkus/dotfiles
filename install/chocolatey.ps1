9d772a619d5df0422e9fe80d9f43dc34e5c133e3
Markus Reiter <me@reitermark.us>
2018-03-14 23:59:29 +0100

    Add programs.

--- install/chocolatey.ps1
+++ install/chocolatey.ps1
@@ -2,3 +2,14 @@ Write-Output "Installing Chocolatey ..."
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco upgrade chocolatey
+
+choco install -y 7zip
+choco install -y autohotkey
+choco install -y cpu-z
+choco install -y gpu-z
+choco install -y msiafterburner
+choco install -y opera
+choco install -y steam
+choco install -y telegram
+choco install -y vcredist2015
+choco install -y visualstudiocode
