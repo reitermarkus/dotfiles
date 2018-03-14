68c9e28990fa60013c465c5376e2a9851750d2d8
Markus Reiter <me@reitermark.us>
2018-03-14 22:26:08 +0100

    Remove even more bloatware.

--- settings/default_apps.ps1
+++ settings/default_apps.ps1
@@ -1,21 +1,31 @@
+# Uninstall useless default apps.
 ForEach($appId in
   "Microsoft.3DBuilder",          # 3D Builder
   "Microsoft.BingFinance",        # Bing Finance
   "Microsoft.BingNews",           # Bing News
   "Microsoft.BingSports",         # Bing Sports
   "Microsoft.BingWeather",        # Bing Weather
+  "king.com.BubbleWitch3Saga",    # Bubble Witch 3 Saga
   "king.com.CandyCrushSodaSaga",  # Candy Crush Soda Saga
+  "*.DisneyMagicKingdoms",        # Disney Magic Kingdoms
+  "*.DragonManiaLegends",         # Dragon Mania Legends
   "*.Facebook",                   # Facebook
-  "Microsoft.MicrosoftOfficeHub", # Get Office and “Get Office365” Notifications
-  "Microsoft.GetStarted",         # Get Started
   "Microsoft.GetHelp",            # Get Help
+  "Microsoft.GetStarted",         # Get Started
+  "*.HiddenCityMysteryofShadows", # Hidden City: Mystery of Shadows
   "Microsoft.WindowsMaps",        # Maps
+  "*.MarchofEmpires",             # March of Empires
   "Microsoft.Messaging",          # Messaging
+  "Microsoft.MinecraftUWP",       # Minecraft Windows 10 Edition
+  "*.Netflix",                    # Netflix
+  "Microsoft.MicrosoftOfficeHub", # Office and “Get Office365” Notifications
   "Microsoft.Office.OneNote",     # OneNote
+  "flaregamesGmbH.RoyalRevolt2",  # Royal Revolt 2
   "Microsoft.SkypeApp",           # Skype
   "*.SlingTV",                    # SlingTV
   "Microsoft.Office.Sway",        # Sway
   "*.Twitter",                    # Twitter
+  "*.Viber",                      # Viber
   "Microsoft.WindowsPhone",       # Windows Phone Companion
   "Microsoft.ZuneMusic",          # Zune Music (Groove)
   "Microsoft.ZuneVideo"           # Zune Video
@@ -24,9 +34,11 @@ ForEach($appId in
   Get-AppXProvisionedPackage -Online | Where DisplayNam -like "$appId" | Remove-AppxProvisionedPackage -Online
 }
 
-# Uninstall Windows Media Player
+# Uninstall Windows Media Player.
 Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -NoRestart -WarningAction SilentlyContinue | Out-Null
 
-# Prevent "Suggested Applications" from returning
-if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent")) {New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Type Folder | Out-Null}
+# Prevent “Suggested Applications” from returning.
+if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent")) {
+  New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Type Folder | Out-Null
+}
 Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" "DisableWindowsConsumerFeatures" 1
