5404936426a3acbda6d7b0947d5aa5c81daf96f4
Markus Reiter <me@reitermark.us>
2018-03-15 01:05:54 +0100

    Only install Chocolatey if not already installed.

--- install/chocolatey.ps1
+++ install/chocolatey.ps1
@@ -1,14 +1,19 @@
-Write-Output "Installing Chocolatey ..."
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+if (-Not (Test-Path env:ChocolateyInstall)) {
+  Write-Output "Installing Chocolatey ..."
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
-choco upgrade chocolatey
+choco upgrade -y chocolatey
 
 choco install -y 7zip
 choco install -y autohotkey
+choco install -y cinebench
 choco install -y cpu-z
+choco install -y crystaldiskmark
 choco install -y gpu-z
 choco install -y msiafterburner
 choco install -y opera
+choco install -y prime95
 choco install -y steam
 choco install -y telegram
 choco install -y vcredist2015
