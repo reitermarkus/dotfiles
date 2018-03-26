2593cfcce512ef5702c1bee7953f5bec942b1439
Markus Reiter <me@reitermark.us>
2018-03-26 10:47:12 +0200

    Change Safari search engine (not working).

--- include/settings/safari.sh
+++ include/settings/safari.sh
@@ -17,9 +17,18 @@ defaults_safari() {
   /usr/bin/defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 
   # Set “DuckDuckGo” as Default Search Provider
-  /usr/bin/defaults write com.apple.Safari SearchProviderIdentifier -string 'com.duckduckgo'
+  /usr/bin/defaults write -g NSPreferredWebServices -dict-add NSWebServicesProviderWebSearch '''
+    <dict>
+      <key>NSDefaultDisplayName</key>
+      <string>DuckDuckGo</string>
+      <key>NSProviderIdentifier</key>
+      <string>com.duckduckgo</string>
+    </dict>
+  '''
 
   # Enable Search Suggestions
   /usr/bin/defaults write com.apple.Safari SuppressSearchSuggestions -bool false
+
+  /usr/bin/killall cfprefsd &>/dev/null
 
 }
