6cd3a128630391bdd493416d53ca1a11a6df1073
Markus Reiter <me@reitermark.us>
2018-04-12 14:50:04 +0200

    Add CS:GO training config.

--- include/settings/steam.sh
+++ include/settings/steam.sh
@@ -6,4 +6,20 @@ defaults_steam() {
     /usr/bin/osascript -e 'tell application "System Events" to delete login item "Steam"'
   fi
 
+  {
+    echo 'sv_cheats 1'
+    echo 'mp_maxmoney 50000'
+    echo 'mp_startmoney 50000'
+    echo 'mp_freezetime 1'
+    echo 'mp_roundtime_defuse 40'
+    echo 'mp_roundtime_hostage 40'
+    echo 'mp_buy_anywhere 1'
+    echo 'mp_buytime 1000'
+    echo 'sv_infinite_ammo 1'
+    echo 'mp_warmup_end'
+    echo 'god'
+    echo 'sv_grenade_trajectory 1'
+    echo 'bind n noclip'
+  } > "${HOME}/Library/Application Support/Steam/userdata/46026291/730/local/cfg/training.cfg"
+
 }
