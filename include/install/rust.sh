7dead4a3905e50f20434e3a62e78b2d697350c06
Markus Reiter <me@reitermark.us>
2018-04-24 21:09:31 +0200

    Install `racer`.

--- include/install/rust.sh
+++ include/install/rust.sh
@@ -3,5 +3,11 @@ install_rustup() {
   rustup-init -y --no-modify-path
   rustup update
 
+  rustup component add rust-src
+
+  if ! which racer &>/dev/null; then
+    cargo install racer
+  fi
+
 }
 
