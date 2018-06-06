d4a5b9e8b5fa150da4cc555e8d154e816c43c9df
Markus Reiter <me@reitermark.us>
2018-06-06 20:00:16 +0200

    Don’t wait for Dropbox sync.

--- include/settings/dropbox.sh
+++ include/settings/dropbox.sh
@@ -16,43 +16,35 @@ link_to_dropbox() {
   # Also move hidden files.
   shopt -s dotglob
 
-  local local_dir="${HOME}/${2-$1}"
-  local dropbox_dir="$(get_dropbox_dir)"
+  local local_dir="${HOME}/${1}"
+  local dropbox_dir="$(get_dropbox_dir)/Sync/~/${1}"
 
-  if test -d "${dropbox_dir}"; then
+  local local_dirname="$(/usr/bin/sed "s|^${HOME}|~|" <<< "${local_dir}")"
+  local dropbox_dirname="$(/usr/bin/sed "s|^${HOME}|~|" <<< "${dropbox_dir}")"
 
-    dropbox_dir="${dropbox_dir}/Sync/~/${1}"
+  if test -L "${dropbox_dir}"; then
+    echo -g "${local_dirname} already linked to Dropbox."
+  else
+    echo -b "Linking ${local_dirname} to ${dropbox_dirname} …"
 
-    local local_dirname="$(/usr/bin/sed "s|^${HOME}|~|" <<< "${local_dir}")"
-    local dropbox_dirname="$(/usr/bin/sed "s|^${HOME}|~|" <<< "${dropbox_dir}")"
-
-    if test -L "${dropbox_dir}"; then
-      echo -g "${local_dirname} already linked to Dropbox."
-    else
-      echo -b "Linking ${local_dirname} to ${dropbox_dirname} …"
-
-      /usr/bin/killall Dropbox &>/dev/null || true
-
-      if ! test -d "${dropbox_dir}"; then
-        /bin/rm -f "${dropbox_dir}"
-      fi
-
-      if ! test -d "${local_dir}"; then
-        /bin/rm -f "${local_dir}"
-      fi
-
-      /bin/mkdir -p "${local_dir}"
-
-      if test -d "${dropbox_dir}"; then
-        /bin/rm -f "${dropbox_dir}/.DS_Store"
-        /usr/bin/find "${dropbox_dir}" -depth 1 -print0 | /usr/bin/xargs -0 -I% /bin/mv -f '%' "${local_dir}/"
-        /bin/rmdir "${dropbox_dir}"
-      fi
-
-      /bin/mkdir -p "$(/usr/bin/dirname "${dropbox_dir}")"
-      /bin/ln -sfn "${local_dir}" "${dropbox_dir}" || echo -r "Error linking ${local_dirname} to ${dropbox_dirname}."
-
+    if ! test -d "${dropbox_dir}"; then
+      /bin/rm -f "${dropbox_dir}"
     fi
+
+    if ! test -d "${local_dir}"; then
+      /bin/rm -f "${local_dir}"
+    fi
+
+    /bin/mkdir -p "${local_dir}"
+
+    if test -d "${dropbox_dir}"; then
+      /bin/rm -f "${dropbox_dir}/.DS_Store"
+      /usr/bin/find "${dropbox_dir}" -depth 1 -print0 | /usr/bin/xargs -0 -I% /bin/mv -f '%' "${local_dir}/"
+      /bin/rmdir "${dropbox_dir}"
+    fi
+
+    /bin/mkdir -p "$(/usr/bin/dirname "${dropbox_dir}")"
+    /bin/ln -sfn "${local_dir}" "${dropbox_dir}"
   fi
 }
 
@@ -61,31 +53,9 @@ dropbox_link_folders() {
 
   # Check if Dropbox has finished syncing.
 
-  echo -b 'Waiting for Dropbox to finish syncing …'
+  echo -b 'Setting up Dropbox symlinks …'
 
-  # Get Dropbox Localizations
-  dropbox_garcon="$(/usr/bin/mdfind -onlyin / kMDItemCFBundleIdentifier==com.getdropbox.dropbox | /usr/bin/head -n 1)/Contents/PlugIns/garcon.appex/Contents/Resources"
-  dropbox_localizations=''
-
-  if [ -d "${dropbox_garcon}" ]; then
-    for lang in "${dropbox_garcon}"/*.lproj; do
-      if [ ! -z "${dropbox_localizations}" ]; then
-        dropbox_localizations+='|'
-      fi
-
-      dropbox_localizations+="$(/usr/bin/plutil -convert json -o - "${lang}/garcon.strings" | /usr/bin/ruby -e "require 'json'; print JSON.parse(STDIN.read)['BadgeTooltipUptodate']")"
-    done
-  fi
-
-  until test -f ~/.dropbox/host.db && \
-        test -d "$(get_dropbox_dir)" && \
-        /usr/bin/osascript -e 'tell application "System Events" to tell application process "Dropbox" to get help of menu bar item 1 of menu bar 2' 2>/dev/null | /usr/bin/tail -n 1 | /usr/bin/grep --quiet -E "${dropbox_localizations}"; do
-    if ! /bin/launchctl list | /usr/bin/grep --quiet -E 'com.getdropbox.dropbox.\d+'; then
-      /usr/bin/open -gjb com.getdropbox.dropbox 2>/dev/null || with_askpass brew cask install dropbox &>/dev/null
-    fi
-
-    /bin/sleep 5
-  done
+  /usr/bin/killall Dropbox &>/dev/null || true
 
   link_to_dropbox 'Desktop'
 
@@ -105,13 +75,6 @@ dropbox_link_folders() {
   link_to_dropbox 'Documents/SketchUp'
   link_to_dropbox 'Documents/Sonstiges'
   link_to_dropbox 'Documents/Uni'
-
-  local favorites='Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.FavoriteItems.sfl'
-  if [ -f "$(get_dropbox_dir)/Sync/~/${favorites}" ]; then
-    /bin/rm -f "${HOME}/${favorites}"
-    /bin/mv "$(get_dropbox_dir)/Sync/~/${favorites}" "${HOME}/${favorites}"
-    /bin/ln -s "${HOME}/${favorites}" "$(get_dropbox_dir)/Sync/~/${favorites}"
-  fi
 
   /usr/bin/open -gja Dropbox
 
