52d0f15384a1c5baa31b27eef2753b11c3230f0e
Markus Reiter <me@reitermark.us>
2018-06-25 06:56:34 +0200

    Simplify and reorder script.

--- .sh
+++ .sh
@@ -3,109 +3,91 @@
 {
 
   # Abort on errors.
-
   set -e
   set -o pipefail
 
-
   # Abort on unbound variables.
-
   set -u
 
-
-  # Disable Ctrl-Z
-
+  # Disable Ctrl-Z.
   trap '' TSTP
 
+  # Trap Ctrl-C.
+  trap 'trap "" INT; echo "\n\033[0;31mAborting …\033[0m" 1>&2; exit 1' INT
 
-  # Prevent System Sleep
-
+  # Prevent system sleep.
   /usr/bin/caffeinate -dimu -w $$ &
 
-
-  # Add commands to run when exiting.
-
+  # Add exit handlers.
   at_exit() {
-    AT_EXIT+="${AT_EXIT:+;}"
+    AT_EXIT+="${AT_EXIT:+$'\n'}"
     AT_EXIT+="${*?}"
     trap "${AT_EXIT}" EXIT
   }
 
   ci() {
-    test -n "${CI+set}"
+    ${CI+true}
+    ${CI-false}
   }
 
   # Accessibility Access
-
   if ! ci && test -z "$(/usr/bin/sqlite3 '/Library/Application Support/com.apple.TCC/TCC.db' \
                        "SELECT * FROM access WHERE client = 'com.apple.Terminal' AND allowed = 1")"; then
-    echo "\033[0;31mPlease enable Accessibility Access for 'Terminal.app' in System Preferences.\033[0m"
+    echo "\033[0;31mPlease enable Accessibility Access for 'Terminal.app' in System Preferences.\033[0m" 1>&2
+    /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility'
     exit 1
   fi
 
-  # Download Repository
-
-  if [ "$(basename "${0}")" != '.sh' ]; then
-    echo '\033[0;34mDownloading Github Repository …\033[0m'
-
-    dotfiles_dir='/tmp/dotfiles-master'
-    /bin/rm -rf "${dotfiles_dir}"
-
-    remove_dotfiles_dir() {
-      echo '\033[0;31mRemoving Dotfiles directory …\033[0m'
-      /bin/rm -rf "${dotfiles_dir}"
-    }
-    at_exit remove_dotfiles_dir
-
-    /usr/bin/curl --progress-bar --location 'https://github.com/reitermarkus/dotfiles/archive/master.zip' | ditto -xk - '/tmp'
-  else
-    dotfiles_dir=$(cd "$(/usr/bin/dirname "$0")" || exit 1; pwd)
-  fi
-
-  cd "${dotfiles_dir}"
-
   if ! ci; then
     # Ask for superuser password, and temporarily add it to the Keychain.
-
-    SUDO_ASKPASS_SCRIPT="$(mktemp)"
-
-    echo \
-      "#!/bin/sh\n" \
-      '/usr/bin/security find-generic-password -s "dotfiles" -a "$USER" -w' \
-      >> "${SUDO_ASKPASS_SCRIPT}"
-
-    /bin/chmod +x "${SUDO_ASKPASS_SCRIPT}"
-
-    delete_askpass_password() {
-      echo '\033[0;31mRemoving password from Keychain …\033[0m'
-      /bin/rm -f "${SUDO_ASKPASS_SCRIPT}"
-      /usr/bin/security delete-generic-password -s 'dotfiles' -a "${USER}"
-    }
-    at_exit delete_askpass_password
-
-    export SUDO_ASKPASS="${SUDO_ASKPASS_SCRIPT}"
-
-    sudo() {
-      /usr/bin/sudo -A "${@}"
-    }
-
     /usr/bin/security add-generic-password -U -s 'dotfiles' -a "${USER}" -w "$(read -s -p "Password: " P < /dev/tty && printf "${P}")"
     printf "\n"
 
-    sudo -k
-    if ! sudo -kv 2>/dev/null; then
-      echo '\033[0;31mIncorrect password. Exiting …\033[0m'
+    at_exit "
+      echo '\033[0;31mRemoving password from Keychain …\033[0m'
+      /usr/bin/security delete-generic-password -s 'dotfiles' -a '${USER}'
+    "
+
+    export SUDO_ASKPASS="$(/usr/bin/mktemp)"
+
+    at_exit "
+      echo '\033[0;31mDeleting SUDO_ASKPASS script …\033[0m'
+      /bin/rm -f '${SUDO_ASKPASS}'
+    "
+
+    {
+      echo "#!/bin/sh\n"
+      echo "/usr/bin/security find-generic-password -s 'dotfiles' -a '${USER}' -w"
+    } >> "${SUDO_ASKPASS}"
+
+    /bin/chmod +x "${SUDO_ASKPASS}"
+
+    if ! /usr/bin/sudo -A -kv 2>/dev/null; then
+      echo '\033[0;31mIncorrect password.\033[0m' 1>&2
       exit 1
     fi
   fi
 
+  # Use local version or download repository.
+  if [ "$(/usr/bin/basename "${0}")" = '.sh' ]; then
+    cd "$(/usr/bin/dirname "$0")"
+  else
+    echo '\033[0;34mDownloading Github Repository …\033[0m'
 
-  # Trap Ctrl-C
-  trap 'trap "" INT; echo "\n\033[0;31mAborting …\033[0m"; exit 1' INT
+    dotfiles_dir="$(/usr/bin/mktemp -d)"
+    pushd "${dotfiles_dir}" >/dev/null
 
+    remove_dotfiles_dir() {
+      echo '\033[0;31mRemoving Dotfiles directory …\033[0m'
+      popd >/dev/null
+      /bin/rm -rf "${dotfiles_dir}"
+    }
+    at_exit remove_dotfiles_dir
 
-  # Run Scripts
+    /usr/bin/curl --progress-bar --location 'https://github.com/reitermarkus/dotfiles/archive/master.tar.gz' | /usr/bin/tar -x --strip-components 1
+  fi
 
+  # Run scripts.
   /usr/bin/rake \
     files \
     xcode:command_line_utilities \
