45235e7e2cd9f0a2aa46c96c192d2ee671b0d421
Markus Reiter <me@reitermark.us>
2018-06-27 16:27:26 +0200

    Fix `respond_to?` for `USER` and `HOME`.

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,20 @@
 require 'etc'
 
 class << USER = BasicObject.new
+  def respond_to?(*)
+    super
+  end
+
   def method_missing(symbol, *args)
     ::Etc.getlogin.public_send(symbol, *args)
   end
 end
 
 class << HOME = BasicObject.new
+  def respond_to?(*)
+    super
+  end
+
   def method_missing(symbol, *args)
     ::File.expand_path('~').public_send(symbol, *args)
   end
