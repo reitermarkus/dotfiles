438316831e1c9da46c1ddec264840ac78c557021
Markus Reiter <me@reitermark.us>
2018-06-27 15:07:29 +0200

    Make `USER` and `HOME` constants dynamic.

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,13 @@
 require 'etc'
 
-USER = Etc.getlogin
-HOME = File.expand_path('~')
+class << USER = BasicObject.new
+  def method_missing(symbol, *args)
+    ::Etc.getlogin.public_send(symbol, *args)
+  end
+end
+
+class << HOME = BasicObject.new
+  def method_missing(symbol, *args)
+    ::File.expand_path('~').public_send(symbol, *args)
+  end
+end
