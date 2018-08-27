e8b02f0ab778a4534e40f8cf44659304d1bb1636
Markus Reiter <me@reitermark.us>
2018-08-27 02:14:58 +0200

    Add `openhab-config.fish`.

---
+++ ~/.config/fish/functions/openhab-config.fish
@@ -0,0 +1,12 @@
+# This file was created automatically, do not edit it directly.
+
+function openhab-config
+  set -l share '/tmp/smb/shares/server.local/Macintosh'
+  set -l pass (security find-internet-password -wa "$USER" -s Server._smb._tcp.local)
+
+  mkdir -p "$share"
+  mount -t smbfs "//$USER:$pass@server.local/Macintosh" "$share" ^ /dev/null
+
+  cd  "$share/usr/local/etc/openhab/"; and \
+  edit "$share/usr/local/etc/openhab/"
+end
