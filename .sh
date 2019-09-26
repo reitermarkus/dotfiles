713a0abe3892874e14d0832d34b9cc5e0c855b19
Markus Reiter <me@reitermark.us>
2019-09-26 22:10:55 +0200

    Run ShellCheck

--- .sh
+++ .sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 {
 
@@ -22,6 +22,7 @@
   at_exit() {
     AT_EXIT+="${AT_EXIT:+$'\n'}"
     AT_EXIT+="${*?}"
+    # shellcheck disable=SC2064
     trap "${AT_EXIT}" EXIT
   }
 
@@ -31,23 +32,18 @@
   }
 
   if ! ci; then
-    if [ "$(echo "$(/usr/bin/sw_vers -productVersion)\n10.14" | /usr/bin/sort -V | /usr/bin/head -n 1)" = '10.14' ]; then
-      # Automation Access
-      osascript -e 'tell application "System Events" to get volume settings' &>/dev/null &
-      pid=$!
-      sleep 0.5
-      kill "${pid}" &>/dev/null || true
-
-      if ! wait "${pid}"; then
-        /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_Automation'
+    if [ "$(printf '%s' "$(/usr/bin/sw_vers -productVersion)\n10.14" | /usr/bin/sort -V | /usr/bin/head -n 1)" = '10.14' ]; then
+      # Full Disk Access
+      if { ! test -r "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" 2>&1; } | grep -q 'Operation not permitted'; then
+        echo 'Add “Terminal.app” to System Preferences -> Security -> Privacy -> Full Disk Access' 1>&2
+        /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles'
+        /usr/bin/open -R /Applications/Utilities/Terminal.app
         exit 1
       fi
 
-      # Full Disk Access
-      if ( ! ls ~/Library/Containers/com.apple.Safari 2>&1 ) | grep -q 'Operation not permitted'; then
-        echo 'Add `Terminal.app` to System Preferences -> Security -> Privacy -> Full Disk Access' 1>&2
-        /usr/bin/open 'x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles'
-        exit 1
+      if test -z "$(/usr/bin/sqlite3 "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" \
+                      "SELECT * FROM access WHERE client = 'com.apple.Terminal' AND indirect_object_identifier = 'com.apple.systemevents' AND allowed = 1")"; then
+        /usr/bin/sqlite3 "${HOME}/Library/Application Support/com.apple.TCC/TCC.db" "REPLACE INTO access VALUES('kTCCServiceAppleEvents','com.apple.Terminal',0,1,1,X'fade0c000000003000000001000000060000000200000012636f6d2e6170706c652e5465726d696e616c000000000003',NULL,0,'com.apple.systemevents',X'fade0c000000003400000001000000060000000200000016636f6d2e6170706c652e73797374656d6576656e7473000000000003',0,$(/bin/date +%s))"
       fi
     else
       # Accessibility Access
@@ -61,7 +57,7 @@
     fi
 
     # Ask for superuser password, and temporarily add it to the Keychain.
-    /usr/bin/security add-generic-password -U -s 'dotfiles' -a "${USER}" -w "$(read -s -p "Password: " P < /dev/tty && printf "${P}")"
+    /usr/bin/security add-generic-password -U -s 'dotfiles' -a "${USER}" -w "$(read -r -s -p "Password: " P < /dev/tty && echo "${P}")"
     printf "\n"
 
     at_exit "
@@ -69,7 +65,7 @@
       /usr/bin/security delete-generic-password -s 'dotfiles' -a '${USER}'
     "
 
-    export SUDO_ASKPASS="$(/usr/bin/mktemp)"
+    SUDO_ASKPASS="$(/usr/bin/mktemp)"
 
     at_exit "
       echo '\033[0;31mDeleting SUDO_ASKPASS script …\033[0m'
@@ -77,11 +73,13 @@
     "
 
     {
-      echo "#!/bin/sh\n"
+      echo "#!/bin/sh"
       echo "/usr/bin/security find-generic-password -s 'dotfiles' -a '${USER}' -w"
-    } >> "${SUDO_ASKPASS}"
+    } > "${SUDO_ASKPASS}"
 
     /bin/chmod +x "${SUDO_ASKPASS}"
+
+    export SUDO_ASKPASS
 
     if ! /usr/bin/sudo -A -kv 2>/dev/null; then
       echo '\033[0;31mIncorrect password.\033[0m' 1>&2
