ace405ad3541388fc9589e5c4e1b35b2559f036e
Markus Reiter <me@reitermark.us>
2020-03-19 02:05:12 +0100

    Create `default` task.

--- .sh
+++ .sh
@@ -110,67 +110,7 @@
   fi
 
   # Run scripts.
-  /usr/bin/rake \
-    files \
-    xcode:command_line_utilities \
-    brew \
-    ruby \
-    rust \
-    python \
-    mas \
-    xcode:accept_license \
-    fonts \
-    mackup \
-    local_scripts \
-    bash \
-    fish \
-    git \
-    github \
-    xcode:defaults \
-    itunes \
-    rapidclick \
-    hazel \
-    virtualbox \
-    steam \
-    teamspeak \
-    csgo \
-    cities_skylines \
-    tex \
-    ccache \
-    sccache \
-    make \
-    pam \
-    keyboard \
-    ui \
-    rocket \
-    safari \
-    locale \
-    startup \
-    transmission \
-    locate_db \
-    menubar \
-    mediathekview \
-    deliveries \
-    tower \
-    bettersnaptool \
-    screensaver \
-    parallels \
-    vagrant \
-    telegram \
-    finder \
-    crash_reporter \
-    dnsmasq \
-    loginwindow \
-    textmate \
-    dock \
-    softwareupdate \
-    terminal \
-    rfc \
-    x11 \
-    z \
-    keka \
-    arduino \
-    mouse_trackpad
+  /usr/bin/rake
 
   at_exit 'echo "\033[0;30mDone.\033[0m"'
 
