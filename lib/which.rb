7ce8988208ada18b8815052f63b4b4c93e28eca7
Markus Reiter <me@reitermark.us>
2021-07-05 01:46:51 +0200

    Show `which` paths on CI.

--- lib/which.rb
+++ lib/which.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+require 'shellwords'
+require 'ci'
+
 def which(executable)
-  ENV['PATH']
+  path = ENV['PATH']
     .split(File::PATH_SEPARATOR)
     .map { |p| "#{p}/#{executable}" }
     .detect { |p| File.executable?(p) }
+
+  $stderr.puts "which(#{executable.inspect}) => #{path.inspect}" if ci?
+
+  path
 end
