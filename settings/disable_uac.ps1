ff600ddca5fd53e86fe85fda88283c142d6014ee
Markus Reiter <me@reitermark.us>
2023-10-03 18:58:05 +0200

    Add commment.

--- settings/disable_uac.ps1
+++ settings/disable_uac.ps1
@@ -5,5 +5,6 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "1"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value "0"
 
+# Disable Win+L shortcut so AutoHotKey can remap it.
 New-Item "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Force
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableLockWorkstation" -Value "1" -Type DWORD
