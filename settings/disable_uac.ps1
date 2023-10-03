3bef466188670007f08d80a33d3879338be9231f
Markus Reiter <me@reitermark.us>
2023-10-03 18:48:33 +0200

    Fix disabling Win+L shortcut.

--- settings/disable_uac.ps1
+++ settings/disable_uac.ps1
@@ -4,4 +4,6 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorUser" -Value "0"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "1"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value "0"
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableLockWorkstation" -Value "1"
+
+New-Item "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableLockWorkstation" -Value "1" -Type DWORD
